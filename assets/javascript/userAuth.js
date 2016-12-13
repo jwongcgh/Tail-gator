3a349b37341cb4d6c6a7d747731b2b3e2b1c2261
yashaRO <yashanyou@gmail.com>
2016-12-12 21:00:12 -0600

    Testing popup/added Sign In/Up option

---
+++ assets/javascript/userAuth.js
@@ -0,0 +1,23 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBMWTK4u_9IMZLWtQMVO3YYWoxGCdXZdsc",
+    authDomain: "prown-e0b61.firebaseapp.com",
+    databaseURL: "https://prown-e0b61.firebaseio.com",
+    storageBucket: "prown-e0b61.appspot.com",
+    messagingSenderId: "709868742724"
+  };
+  firebase.initializeApp(config);
+var database = firebase.database()
+var auth = firebase.auth()
+
+$(document).ready(function() {
+    var provider = new firebase.auth.FacebookAuthProvider();
+    provider.addScope('email');
+provider.addScope('user_friends');
+    auth.signInWithPopup(provider);
+    document.getElementById('submit').addEventListener('click', function() {
+        var email = document.getElementById('email').value
+        var pw = document.getElementById('password').value        
+        auth.signInWithEmailAndPassword(email, pw).catch(e => console.log(e.message))
+    })
+})
