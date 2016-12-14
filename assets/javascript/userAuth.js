073e75b20cfc77b8872ce49a5acfbecc4e480e55
yashaRO <yashanyou@gmail.com>
2016-12-14 01:19:34 -0600

    Testing out auth->database connection

--- assets/javascript/userAuth.js
+++ assets/javascript/userAuth.js
@@ -14,14 +14,16 @@ $(document).ready(function() {
     
     document.getElementById('facebook').onclick = function() {
         var provider = new firebase.auth.FacebookAuthProvider();
-        auth.signInWithPopup(provider);
+        auth.signInWithPopup(provider).then(function(result) {
+            var token = result.credential.accessToken;
+            var user = result.user;
+        });
     }
     document.getElementById('googleP').onclick = function() {
         var provider = new firebase.auth.GoogleAuthProvider();
         auth.signInWithPopup(provider).then(function(result) {
             var token = result.credential.accessToken;
             var user = result.user;
-            console.log(token,user)
         });
     }
     document.getElementById('submit').addEventListener('click', function() {
@@ -29,4 +31,16 @@ $(document).ready(function() {
         var pw = document.getElementById('password').value        
         auth.signInWithEmailAndPassword(email, pw).catch(e => console.log(e.message))
     })
+	var ref = new Firebase("https://<YOUR-FIREBASE-APP>.firebaseio.com");
+    ref.onAuth(function(authData) {
+		if (authData && isNewUser) {
+        // save the user's profile into Firebase so we can list users,
+        // use them in Security and Firebase Rules, and show profiles
+        ref.child("users").child(authData.uid).set({
+			provider: authData.provider,
+			name: getName(authData)
+			//some more user data
+		});
+	  }
+	});
 })
