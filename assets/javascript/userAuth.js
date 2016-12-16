3ee9a5f4d616f1a838c32b606b4e6514179a38d2
yashaRO <yashanyou@gmail.com>
2016-12-16 15:06:35 -0600

    signup makeover, added input checkers, change Firebase set

--- assets/javascript/userAuth.js
+++ assets/javascript/userAuth.js
@@ -10,32 +10,75 @@
 var database = firebase.database()
 var auth = firebase.auth()
 var signup = function() {
-	name = document.getElementById('sub_name').value;
-	email = document.getElementById('sub_email').value;
-	zip = document.getElementById('sub_zip').value;
-	pw = document.getElementById('sub_password').value;
-	city = document.getElementById('sub_city').value;
-	address = document.getElementById('sub_address').value;
-    if (zip.length > 5) {console.log ('nah'); return}
-    console.log(zip.length)
-    console.log(name,email,zip,pw,address,city)
+	var fname = document.getElementById('sub_fname').value.toUpperCase();
+	var lname = document.getElementById('sub_lname').value.toUpperCase();
+	var email = document.getElementById('sub_email').value.toUpperCase();
+	var phone = document.getElementById('sub_phone').value;
+	var zip = document.getElementById('sub_zip').value;
+	var pw = document.getElementById('sub_password').value;
+	var city = document.getElementById('sub_city').value.toUpperCase();
+	var state = document.getElementById('sub_state').value.toUpperCase();
+	var address = document.getElementById('sub_address').value.toUpperCase().match(/\d+\s[A-Za-z]+/);
+    var errors = false;
+    $('.signup input').each(function() {
+        if (!this.value) {
+            $(this).addClass('error');
+            errors = true;
+        }
+        if (this.value && $(this).hasClass('error')) {
+            $(this).removeClass('error');
+        }
+    })
+    if (!address) {
+        $('#sub_address').addClass('error');
+        errors = true;
+    }
+    if (!email.match(/\@MAIL\.COM/)) {
+        $('#sub_email').addClass('error');
+        errors = true;
+    }
+    if (pw.length < 6) {
+        $('#sub_password').addClass('error');
+        errors = true;
+    }
+    if (state.length < 2) {
+        $('#sub_state').addClass('error');
+        errors = true;
+    }
+    if (city.length < 4) {
+        $('#sub_city').addClass('error');
+        errors = true;
+    }
+    if (errors) {return}
+	
     database.ref('users/' + email.replace(/\.\w+/,'')).set({
-        name: name,
-        address: address,
-        city: city,
-        zip: zip
+        fname: fname,
+		lname: lname,
+		phone: phone,
+        email: email,
+        address_info: {
+			address: address,
+			city: city,
+			state: state,
+			zip: zip
+		}
     })
     auth.createUserWithEmailAndPassword(email, pw).catch(function(error) {
         var errorCode = error.code;
         var errorMessage = error.message;
     })
-    $('#sub_signup').prop('disabled',true)
+	window.location.assign('index.html')
 }
 var signOut = function() {
-    auth.signOut()
-    location.reload()
+    auth.signOut();
+    location.reload();
 }
 $(document).ready(function() {
+	
+	//Start sign up mod
+	$('#sub_phone').mask('(999) 999-9999');
+	$('#sub_zip').mask('99999');
+	//End sign up mod
 
     document.getElementById('facebook').onclick = function() {
         var provider = new firebase.auth.FacebookAuthProvider();
@@ -52,17 +95,17 @@ $(document).ready(function() {
         });
     }
     document.getElementById('submit').addEventListener('click', function() {
-        var email = document.getElementById('loginEmail').value
-        var pw = document.getElementById('loginPW').value        
+        var email = document.getElementById('loginEmail').value;
+        var pw = document.getElementById('loginPW').value; 
         auth.signInWithEmailAndPassword(email, pw).catch(e => console.log(e.message))
     })
     auth.onAuthStateChanged(function(user) {
         if(user) {
-            $('.signinTab').hide()
-            $('#signoutTab').show()
+            $('.signinTab').hide();
+            $('#signoutTab').show();
         } else {
             if(window.location.pathname.match('/profile.html')) {
-                window.location.assign('index.html')
+                window.location.assign('index.html');
             }
         }
     })
